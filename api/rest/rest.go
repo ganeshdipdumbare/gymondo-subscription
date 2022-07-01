e871e8953ea8aad8629936c2f4f2c2ea095ab893
Ganeshdip Dumbare <ganeshdip@12traits.com>
2022-07-01 07:32:06 +0200

    change start server code

--- api/rest/rest.go
+++ api/rest/rest.go
@@ -5,9 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/ganeshdipdumbare/gymondo-subscription/api"
@@ -54,13 +51,6 @@ func (a *apiDetails) StartServer() {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-
-	log.Println("Shutting down server...")
-	a.GracefulStopServer()
 }
 
 // GracefulStopServer gracefully stops the rest server
